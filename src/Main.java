49d02af84dd2355e6079a413e35f103dce8d6ff3
Вячеслав <friendcw@mail.ru>
2022-12-05 00:02:37 +0400

    HW Collections lesson 2 sets

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product banan = new Product("Банан", 17f, 1);
+        Product ananas = new Product("Ананас", 89f, 1);
+        ProductList productList = new ProductList();
+        productList.addProduct(banan);
+        productList.addProduct(ananas);
+        System.out.println(productList);
+        numbersSetRemoveEven();
+
+    }
+
+    private static void numbersSetRemoveEven() {
+        Set<Integer> numbers = new HashSet<>(20);
+        Random random = new Random();
+        while (numbers.size() < 20) {
+            numbers.add(random.nextInt(1000));
+        }
+        System.out.println(numbers);
+        Iterator<Integer> numberIterator = numbers.iterator();
+        while (numberIterator.hasNext()) {
+            int number = numberIterator.next();
+            if (number % 2 != 0) {
+                numberIterator.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 }
